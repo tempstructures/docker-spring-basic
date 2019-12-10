1d534533a0de117c53c89003138c7042c66a2670
tempstructures <58729760+tempstructures@users.noreply.github.com>
2019-12-10 11:27:29 +0000

    Update ErrorScenarios.java

--- src/main/java/com/tempstructure/hello/ErrorScenarios.java
+++ src/main/java/com/tempstructure/hello/ErrorScenarios.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ErrorScenarios {
 	@GetMapping
 	public String error() {
-		return "oopsiee!!!!";
+		return "oopsiee!!!!!!";
 	}
 }
